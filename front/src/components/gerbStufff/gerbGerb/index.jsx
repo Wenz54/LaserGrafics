1a6fedcc2810034f578ae39246d4e3eeb0585708
Wenz54 <wenz5455@gmail.com>
2025-06-21 03:42:42 +0300

    dd

--- front/src/components/gerbStufff/gerbGerb/index.jsx
+++ front/src/components/gerbStufff/gerbGerb/index.jsx
@@ -144,7 +144,7 @@ const Gerb1 = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:5000/api/add-to-cart', {
+            const response = await fetch('${process.env.REACT_APP_API_URL}/api/add-to-cart', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
